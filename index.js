13a69d80d720b2046d22a6422b258dec146b94b6
Kimberly <youneedanemail@gmail.com>
2024-11-05 16:53:27 +0000

    Updated HTML pg 35

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ let app = new express();
 const knex = require("knex")({
  client: "mysql",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host:"concert-db.chi48m68g7t4.us-east-2.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
+  password: "DonutPassw",
   database:"paradise-concerts",
   port: 3306,
  },
 });
 
 app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
- .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+knex
+.select()
+.from("venues")
+.then((result) => {
+let html = "<body><ul>";
+for (let i=0;i<result.length;i++) {
+html += "<li>" + result[i].location + "</li>";
+}
+html += "</body>"
+res.send(html);
+});
 });
 app.listen(3000);
